2f947d142a4096474f808b103c1b9da304c9e26c
popak2060-png <popak2060@gmail.com>
2026-07-02 23:57:47 +0330

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "expense-app-v1";
+const CACHE_NAME = "expense-app-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -17,5 +17,19 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
 });
